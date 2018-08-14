2a7166dbf7e22ea4a732c3c4f6259b6e8b8141ed
sergo2 <41687826+sergo2@users.noreply.github.com>
2018-08-14 17:49:00 +0300

    switched to SDL file format

--- bul_test.py
+++ bul_test.py
@@ -1,9 +1,11 @@
-import pytest
 import pandas as pd
+from source_config import * 
 
-bul_file_1 = "C:\\Python_progs\\bul_tests\\bul1.sdl"
-bul_file_2 = ""
+bul_file_sdl = path_to_report_files + "#20180810#_MOEX.SDL"
 
-bul_df_1 = pd.read_csv(bul_file_1, sep='\t')
-print(bul_df_1.shape)
+df_sdl = pd.read_csv(bul_file_sdl, sep='\t', engine='python', skipfooter = 1)
+# Replace blank spaces
+df_sdl.columns = df_sdl.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
 
+for i in df_sdl.columns:
+    print(i)
